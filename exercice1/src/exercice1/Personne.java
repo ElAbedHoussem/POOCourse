33beb78a2dc6a5e2efe3b43223307cd984b9f009
ElAbedHoussem <el.abed.houssem94@gmail.com>
2019-02-06 18:56:19 +0100

    first exercice using the tow methods : using string an d using SampleDateFormat

---
+++ exercice1/src/exercice1/Personne.java
@@ -0,0 +1,27 @@
+package exercice1;
+
+public class Personne {
+
+	private String nom;
+	private String prenom;
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	private String dateOfBirth;
+	
+}
